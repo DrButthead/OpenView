6cf857eb44b54e69f5a5b2b59cda05db385ac367
Dan <danbarry16@googlemail.com>
2020-04-22 15:18:21 +1200

    Basic Main class

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+package b.ov;
+
+/**
+ * Main.java
+ *
+ * Parse the command line arguments are start execution as required.
+ **/
+public class Main{
+  /**
+   * main()
+   *
+   * Entry point into the program that starts an instance of Main.
+   *
+   * @param args The command line arguments.
+   **/
+  public static void main(String[] args){
+    new Main(args);
+  }
+
+  /**
+   * Main()
+   *
+   * Parse the command line arguments and perform the conversion as required.
+   *
+   * @param args The command line arguments.
+   **/
+  public Main(String[] args){
+    /* TODO: Parse command line parameters. */
+  }
+}
